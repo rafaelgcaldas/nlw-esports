4fc9ce114582e8b5e5c82565dbf7d775f80229bc
Rafael <rafael.gcaldas01@gmail.com>
2022-09-20 22:41:22 -0300

    Linear gradient

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logo} alt="Logo" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+      </h1>
     </div>
   )
 }
